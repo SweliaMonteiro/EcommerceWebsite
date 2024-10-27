74fe9ac66c13196db7109094323e652923b76b30
sweliamonteiro <sweliamonteiro05@gmail.com>
2024-10-27 17:32:18 +0530

    UserService

--- UserService/src/main/java/com/example/controllers/UserController.java
+++ UserService/src/main/java/com/example/controllers/UserController.java
@@ -8,9 +8,12 @@ import com.example.exceptions.UserNotFoundException;
 import com.example.models.Token;
 import com.example.models.User;
 import com.example.services.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,19 +24,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // Used to send messages to Kafka where Key is the Topic and Value is the user details which will be used by Notification service
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    // This ObjectMapper is used to convert the Order Dto object to a JSON string as Kafka only accepts strings as messages and not objects
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     // This method registers a user and returns the user details after saving them in the database
     @PostMapping("/register")
-    public ResponseEntity<UserDto> register(@RequestBody SignUpRequestDto signUpRequestDto) throws UserAlreadyExistsException {
+    public ResponseEntity<UserDto> register(@RequestBody SignUpRequestDto signUpRequestDto) throws UserAlreadyExistsException, JsonProcessingException {
         User user = userService.register(signUpRequestDto.getName(), signUpRequestDto.getEmail(), signUpRequestDto.getPassword());
+        // Send the UserDto object as a JSON string to the Kafka topic "NotifyUserRegister"
+        kafkaTemplate.send("NotifyUserRegister", objectMapper.writeValueAsString(UserDto.from(user)));
         return new ResponseEntity<>(UserDto.from(user), HttpStatus.OK);
     }
 
 
     // This method is used to log in a user and return a token for the user to access the application resources
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> logIn(@RequestBody LogInRequestDto logInRequestDto) throws UserNotFoundException, PasswordNotMatchingException {
+    public ResponseEntity<TokenDto> logIn(@RequestBody LogInRequestDto logInRequestDto) throws UserNotFoundException, PasswordNotMatchingException, JsonProcessingException {
         Token token = userService.logIn(logInRequestDto.getEmail(), logInRequestDto.getPassword());
+        // Send the LogInRequestDto object as a JSON string to the Kafka topic "NotifyUserLogIn"
+        kafkaTemplate.send("NotifyUserLogIn", objectMapper.writeValueAsString(logInRequestDto));
         return new ResponseEntity<>(TokenDto.from(token), HttpStatus.OK);
     }
 
@@ -60,8 +75,10 @@ public class UserController {
 
     // This method updates the user profile and returns the updated user details
     @PutMapping("/updateProfile/{id}")
-    public ResponseEntity<UserDto> updateProfile(@PathVariable long id, @RequestBody UpdateProfileRequestDto updateProfileRequestDto) throws UserNotFoundException {
+    public ResponseEntity<UserDto> updateProfile(@PathVariable long id, @RequestBody UpdateProfileRequestDto updateProfileRequestDto) throws UserNotFoundException, JsonProcessingException {
         User user = userService.updateProfile(id, updateProfileRequestDto.getName(), updateProfileRequestDto.getEmail(), updateProfileRequestDto.getPassword());
+        // Send the UserDto object as a JSON string to the Kafka topic "NotifyUserUpdateProfile"
+        kafkaTemplate.send("NotifyUserUpdateProfile", objectMapper.writeValueAsString(UserDto.from(user)));
         return new ResponseEntity<>(UserDto.from(user), HttpStatus.OK);
     }
 
